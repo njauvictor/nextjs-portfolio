0b0a813ab3a3a7d0e9a5093870288b652e8fb049
njauvictor <141951816+njauvictor@users.noreply.github.com>
2024-03-12 12:25:50 +0300

    x

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+/* import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
